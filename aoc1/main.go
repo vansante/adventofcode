ffb23863aa9b2e76da79d2bb9b64a46905b8f2ec
Paul <pvansanten@gmail.com>
2017-12-05 22:05:51 +0100

    First!

---
+++ aoc1/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("D:/go/src/adventofcode2017/aoc1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	total := 0
+
+	inputStr := string(input)
+	if len(input) > 0 {
+		prevDigit := -1
+		for i := 0; i < len(input); i++ {
+			curDigit, err := strconv.Atoi(inputStr[i:i+1])
+			if err != nil {
+				panic(err)
+			}
+
+			if curDigit == prevDigit {
+				total += curDigit
+			}
+
+			prevDigit = curDigit
+		}
+
+		firstDigit, err := strconv.Atoi(inputStr[0:1])
+		if err != nil {
+			panic(err)
+		}
+
+		if firstDigit == prevDigit {
+			total += prevDigit
+		}
+	}
+
+	fmt.Printf("Total: %d", total)
+}
