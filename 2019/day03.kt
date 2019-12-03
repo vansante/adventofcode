feb737a0d4601e1456879cb768fa887aaa3eda52
Paul van Santen <pvansanten@transip.nl>
2019-12-03 18:23:30 +0100

    part II

--- 2019/day03.kt
+++ 2019/day03.kt
@@ -10,10 +10,12 @@ fun main() {
     val coordinates = mutableMapOf<String, Int>()
     var closest = Int.MAX_VALUE
     var wireID = 1
-    
+    var lowestMeetup = Int.MAX_VALUE
+    val steps = mutableMapOf<String, Int>()
     wires.forEach { wire ->
         var x = 0
         var y = 0
+        var step = 0
         coordinates[coord(0, 0)] = wireID
         wire.forEach { direction ->
             var delta = direction.substring(1).toInt()
@@ -26,15 +28,26 @@ fun main() {
                     }
                     val origX = x
                     while (true) {
-                        val closer = checkCoordinateDistance(coordinates, wireID, x, y)
+                        if (wireID == 1) {
+                            steps[coord(x, y)] = step
+                        }
+                        val closer = checkCollision(coordinates, wireID, x, y)
                         if (closer in 0 until closest) {
                             closest = closer
+                        }
+
+                        if (closer > 0) {
+                            val totalSteps = steps[coord(x, y)]!! + step
+                            if (totalSteps < lowestMeetup) {
+                                lowestMeetup = totalSteps
+                            }
                         }
 
                         if (x == origX + delta) {
                             break
                         }
                         x += inc
+                        step++
                     }
                 }
                 "L", "R" -> {
@@ -44,15 +57,26 @@ fun main() {
                     }
                     val origY = y
                     while (true) {
-                        val closer = checkCoordinateDistance(coordinates, wireID, x, y)
+                        if (wireID == 1) {
+                            steps[coord(x, y)] = step
+                        }
+                        val closer = checkCollision(coordinates, wireID, x, y)
                         if (closer in 0 until closest) {
                             closest = closer
+                        }
+
+                        if (closer > 0) {
+                            val totalSteps = steps[coord(x, y)]!! + step
+                            if (totalSteps < lowestMeetup) {
+                                lowestMeetup = totalSteps
+                            }
                         }
 
                         if (y == origY + delta) {
                             break
                         }
                         y += inc
+                        step++
                     }
                 }
             }
@@ -61,9 +85,10 @@ fun main() {
     }
 
     println("Part I: Closest is $closest")
+    println("Part II: Lowest meetup is $lowestMeetup")
 }
 
-fun checkCoordinateDistance(coordinates: MutableMap<String, Int>, wireID: Int, x: Int, y: Int): Int {
+fun checkCollision(coordinates: MutableMap<String, Int>, wireID: Int, x: Int, y: Int): Int {
     val wire = coordinates[coord(x, y)]
     coordinates[coord(x, y)] = wireID
     if (wire == null || wire == wireID) {
