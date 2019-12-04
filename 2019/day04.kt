baad9ae141bd51ca6892d8f2511b5f79e0b9981b
Paul van Santen <pvansanten@transip.nl>
2019-12-04 08:56:15 +0100

    Part 2 wip

---
+++ 2019/day04.kt
@@ -0,0 +1,46 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val inputLower = 273025
+    val inputUpper = 767253
+
+    var part1Matches = 0
+    var part2Matches = 0
+
+    for (i in inputLower..inputUpper) {
+        var highest = 0
+        var match = true
+        var doubleChar = false
+        var lastChar = 'a' // never matches
+        var charMatches = 0
+         i.toString().forEach loop@{ char ->
+            if (char.toInt() < highest) {
+                match = false
+                return@loop
+            }
+            highest = char.toInt()
+            if (lastChar == char) {
+                charMatches++
+//                if (charMatches > 2) {
+//
+//                }
+            } else {
+                if (charMatches == 2) {
+                    doubleChar = true
+                }
+                charMatches = 0
+            }
+            lastChar = char
+        }
+        if (charMatches == 2) {
+            doubleChar = true
+        }
+        if (match && doubleChar) {
+            part1Matches++
+        }
+    }
+
+    println("Part I: Total matches: $part1Matches")
+    println("Part II: Total matches: $part1Matches")
+}
