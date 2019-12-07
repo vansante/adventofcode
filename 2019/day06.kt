e608a3d407a48b599f08c4e7a66ff370e777b09d
Paul van Santen <pvansanten@gmail.com>
2019-12-07 20:53:38 +0100

    Cleanup

--- 2019/day06.kt
+++ 2019/day06.kt
@@ -1,11 +1,9 @@
 import java.io.File
 
 fun main() {
-//    val childToParent = mutableMapOf<String, MutableCollection<String>>()
     val parentToChild = mutableMapOf<String, String>()
     File("day06.txt").forEachLine {
         val input = it.split(")")
-//        childToParent[input[0]]!!.add(input[1])
         parentToChild[input[1]] = input[0]
     }
 
@@ -14,7 +12,6 @@ fun main() {
         var orbit = parent
         while (orbit != "COM") {
             orbitCount++
-
             orbit = parentToChild[orbit]!!
         }
     }
@@ -27,7 +24,6 @@ fun main() {
     while (orbit != "COM") {
         myCount++
         visited[orbit] = myCount
-        println(orbit)
         orbit = parentToChild[orbit]!!
     }
 
@@ -39,7 +35,6 @@ fun main() {
             break
         }
         sanCount++
-        println(orbit)
         orbit = parentToChild[orbit]!!
     }
 
