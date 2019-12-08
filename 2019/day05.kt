ce13791670e64951c9409bf560e8040c7db7f833
Paul van Santen <pvansanten@gmail.com>
2019-12-08 11:35:54 +0100

    day 05 pt1

--- 2019/day05.kt
+++ 2019/day05.kt
@@ -3,18 +3,22 @@ import java.lang.Exception
 import java.nio.file.Paths
 
 fun main() {
-    val input = File("day02.txt").readText()
+    val input = File("day05.txt").readText()
     val numbers = input.splitToSequence(",").map { it.toInt() }.toList().toIntArray()
 
-    val machine = Machine(memory = numbers)
+    val machine = Machine(memory = numbers, input = 1)
     machine.run()
+    println("Part I: Answer is: ${machine.output}")
 }
 
 
-class Machine(private val memory: IntArray) {
+class Machine(private val memory: IntArray, private val input: Int) {
+    var output = Int.MIN_VALUE
+
     fun run() {
         var idx = 0
         while (idx < memory.count()) {
+            println("executing idx $idx : ${memory[idx]}")
             idx = execute(idx)
         }
     }
@@ -25,20 +29,31 @@ class Machine(private val memory: IntArray) {
             1 -> {
                 val value1 = getValue(isModeImmediate(instruction, 2), idx + 1)
                 val value2 = getValue(isModeImmediate(instruction, 1), idx + 2)
+                println("add: $value1 + $value2")
                 setValue(memory[idx+3], value1 + value2)
                 return idx + 4
             }
             2 -> {
                 val value1 = getValue(isModeImmediate(instruction, 2), idx + 1)
                 val value2 = getValue(isModeImmediate(instruction, 1), idx + 2)
+                println("multiply: $value1 * $value2")
                 setValue(memory[idx+3], value1 * value2)
                 return idx + 4
+            }
+            3 -> {
+                val param = getValue(true, idx + 1)
+                setValue(param, input)
+                return idx + 2
+            }
+            4 -> {
+                output = getValue(isModeImmediate(instruction, 2), idx + 1)
+                return idx + 2
             }
             99 -> {
                 return Int.MAX_VALUE
             }
         }
-        throw Exception("invalid opcode at idx $idx: ${getOpcode(idx)}")
+        throw Exception("invalid opcode at idx $idx: ${getOpcode(instruction)}")
     }
 
     private fun getValue(immediate: Boolean, address: Int): Int {
@@ -50,6 +65,7 @@ class Machine(private val memory: IntArray) {
     }
 
     private fun setValue(address: Int, value: Int) {
+        println("storing $address -> $value")
         memory[address] = value
     }
 
