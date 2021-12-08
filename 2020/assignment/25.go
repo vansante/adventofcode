d9ba12bb75f59a8ec817aa1411e5ab5243b1e1e4
Paul van Santen <pvansanten@transip.nl>
2021-12-08 15:14:45 +0100

    Day 25

--- 2020/assignment/25.go
+++ 2020/assignment/25.go
@@ -1,14 +1,66 @@
 package assignment
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Day25 struct{}
 
+func (d *Day25) getKeys(input string) (int64, int64) {
+	split := strings.Split(input, "\n")
+
+	cardPK, err := strconv.ParseInt(split[0], 10, 32)
+	CheckErr(err)
+
+	doorPK, err := strconv.ParseInt(split[1], 10, 32)
+	CheckErr(err)
+	return cardPK, doorPK
+}
+
+func (d *Day25) transformOnce(val, subject int64) int64 {
+	return (val * subject) % 20201227
+}
+
+func (d *Day25) transform(subject int64, loopSz int) int64 {
+	val := int64(1)
+	for i := 0; i <= loopSz; i++ {
+		val = d.transformOnce(val, subject)
+	}
+	return val
+}
+
+func (d *Day25) findLoopSize(subject, publicKey int64) int {
+	const trialSize = 100_000_000
+	val := int64(1)
+	for i := 0; i < trialSize; i++ {
+		val = d.transformOnce(val, subject)
+		if val == publicKey {
+			return i
+		}
+	}
+	panic("no loop size found")
+}
+
 func (d *Day25) SolveI(input string) int64 {
-	// TODO: FIXME: Implement me!
-	panic("no result")
+	cardPK, doorPK := d.getKeys(input)
+
+	cardLoopSz := d.findLoopSize(7, cardPK)
+	doorLoopSz := d.findLoopSize(7, doorPK)
+
+	fmt.Println(cardLoopSz, doorLoopSz)
+
+	encKey1 := d.transform(doorPK, cardLoopSz)
+	encKey2 := d.transform(cardPK, doorLoopSz)
+
+	if encKey1 != encKey2 {
+		panic("encryption keys are not equal")
+	}
+
+	return encKey1
 }
 
 func (d *Day25) SolveII(input string) int64 {
-	// TODO: FIXME: Implement me!
-	panic("no result")
+	return 0
 }
-
