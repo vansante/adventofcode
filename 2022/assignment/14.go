a65685b946f17128f8209c8592790ed4e4b0572c
Paul van Santen <pvansanten@transip.nl>
2022-12-13 13:21:35 +0100

    Add day 14 skeleton

---
+++ 2022/assignment/14.go
@@ -0,0 +1,11 @@
+package assignment
+
+type Day14 struct{}
+
+func (d *Day14) SolveI(input string) any {
+	return "Not Implemented Yet"
+}
+
+func (d *Day14) SolveII(input string) any {
+	return "Not Implemented Yet"
+}
