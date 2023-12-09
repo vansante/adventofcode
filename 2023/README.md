7a6fb124af1ded28def308884a0f0a686e2bf527
Paul van Santen <pvansanten@transip.nl>
2023-12-09 11:55:06 +0100

    Add day 7 pt 1

--- 2023/README.md
+++ 2023/README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 34.678ms
 
 ```
 Day 07
-Time part 1: -
+Time part 1: 1.755ms
 Time part 2: -
-Both parts: -
+Both parts: 1.755ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 16/50
-Total time: 2157.161ms
+Total stars: 17/50
+Total time: 2158.915ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day07/index.ts
+++ 2023/src/day07/index.ts
@@ -1,11 +1,125 @@
 import run from "aocrunner"
 
-const parseInput = (rawInput: string) => rawInput
+const cardTypes = [
+  "A",
+  "K",
+  "Q",
+  "J",
+  "T",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+].reverse()
 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput)
+interface Hand {
+  cards: string
+  bid: number
+  strength: number
+}
 
-  return
+const parseInput = (rawInput: string): Array<Hand> => {
+  return rawInput.split("\n").map((line: string): Hand => {
+    return {
+      cards: line.substring(0, 5),
+      bid: parseInt(line.substring(6), 10),
+      strength: -1,
+    } as Hand
+  })
+}
+
+const strength = (cards: string): number => {
+  const freqs = new Map<string, number>()
+
+  for (let i = 0; i < cards.length; i++) {
+    if (freqs.has(cards[i])) {
+      freqs.set(cards[i], (freqs.get(cards[i]) as number) + 1)
+    } else {
+      freqs.set(cards[i], 1)
+    }
+  }
+
+  switch (freqs.size) {
+    case 1:
+      return 10 // 5 of a kind
+    case 2:
+      if (freqs.get(cards[0]) == 2 || freqs.get(cards[0]) == 3) {
+        return 8 // full house
+      }
+      return 9 // 4 of a kind
+  }
+
+  let result = null
+  let pairCount = 0
+  freqs.forEach((freq: number, card: string) => {
+    if (freq === 3) {
+      result = 7 // 3 of a kind
+    }
+    if (freq === 2) {
+      pairCount++
+    }
+  })
+
+  if (result) {
+    return result
+  }
+
+  switch (pairCount) {
+    case 2:
+      return 6 // 2 pair
+    case 1:
+      return 5 // pair
+  }
+
+  return 4
+}
+
+const strongerCard = (a: string, b: string): boolean => {
+  for (let i = 0; i < a.length; i++) {
+    const aCard = cardTypes.indexOf(a[i])
+    const bCard = cardTypes.indexOf(b[i])
+    // console.log(a[i], aCard, b[i], bCard)
+
+    if (aCard > bCard) {
+      return true
+    }
+    if (bCard > aCard) {
+      return false
+    }
+  }
+
+  console.error("Same cards!", a, b)
+  return true
+}
+
+const part1 = (rawInput: string): number => {
+  const hands = parseInput(rawInput)
+
+  hands.forEach((h: Hand) => {
+    h.strength = strength(h.cards)
+  })
+
+  const sorted = hands.sort((a: Hand, b: Hand): number => {
+    if (a.strength > b.strength) {
+      return 1
+    }
+    if (b.strength > a.strength) {
+      return -1
+    }
+    return strongerCard(a.cards, b.cards) ? 1 : -1
+  })
+
+  // console.log(sorted)
+  let total = 0
+  sorted.forEach((h: Hand, idx: number) => {
+    total += h.bid * (idx + 1)
+  })
+
+  return total
 }
 
 const part2 = (rawInput: string) => {
