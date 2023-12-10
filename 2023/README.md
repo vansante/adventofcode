9046760b9cb1bbc9bf14384333a8d5280882f62c
Paul van Santen <pvansanten@transip.nl>
2023-12-10 11:17:18 +0100

    day 10 pt 1 complete

--- 2023/README.md
+++ 2023/README.md
@@ -20,7 +20,7 @@
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
-![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%86/yellow)](src/day10)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 34.678ms
 
 ```
 Day 07
-Time part 1: 1.692ms
-Time part 2: 3.412ms
-Both parts: 5.104ms
+Time part 1: 1.802ms
+Time part 2: 1.055ms
+Both parts: 2.858ms
 ```
 
 ```
@@ -132,9 +132,9 @@ Both parts: 2.468ms
 
 ```
 Day 10
-Time part 1: -
+Time part 1: 12.585ms
 Time part 2: -
-Both parts: -
+Both parts: 12.585ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 18/50
-Total time: 2162.265ms
+Total stars: 19/50
+Total time: 2172.604ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day10/index.ts
+++ 2023/src/day10/index.ts
@@ -5,24 +5,30 @@ interface Coord {
   y: number
 }
 
-const cardinals = ['n', 'e', 's', 'w']
+const cardinals = ["n", "e", "s", "w"]
 
 const directions = new Map<string, Coord>()
-directions.set(cardinals[0], {x: 0, y: -1})
-directions.set(cardinals[1], {x: 1, y: 0})
-directions.set(cardinals[2], {x: 0, y: 1})
-directions.set(cardinals[3], {x: -1, y: 0})
+directions.set(cardinals[0], { x: 0, y: -1 })
+directions.set(cardinals[1], { x: 1, y: 0 })
+directions.set(cardinals[2], { x: 0, y: 1 })
+directions.set(cardinals[3], { x: -1, y: 0 })
+
+const opposite = (card: string): string => {
+  const idx = (cardinals.indexOf(card) + 2) % cardinals.length
+  return cardinals[idx]
+}
 
 interface Tile {
   connectors: Map<string, boolean>
   tiles: Map<string, Tile>
-
+  distance: number
   isStart: boolean
 }
 
 interface Input {
   startX: number
   startY: number
+  start: Tile | null
   tiles: Array<Array<Tile>>
 }
 
@@ -32,6 +38,7 @@ const parseInput = (rawInput: string): Input => {
   const input = {
     startX: 0,
     startY: 0,
+    start: null,
     tiles: [],
   } as Input
 
@@ -43,40 +50,44 @@ const parseInput = (rawInput: string): Input => {
       const char = line[x]
       const t = {
         connectors: new Map<string, boolean>(),
+        tiles: new Map<string, Tile>(),
+        distance: Number.MAX_SAFE_INTEGER,
         isStart: false,
       } as Tile
 
       switch (char) {
         case "S":
           t.isStart = true
+          t.distance = 0
           for (const card of cardinals) {
             t.connectors.set(card, true)
           }
           input.startY = y
           input.startX = x
+          input.start = t
         case "|":
-          t.connectors.set('n', true)
-          t.connectors.set('s', true)
+          t.connectors.set("n", true)
+          t.connectors.set("s", true)
           break
         case "-":
-          t.connectors.set('w', true)
-          t.connectors.set('e', true)
+          t.connectors.set("w", true)
+          t.connectors.set("e", true)
           break
         case "L":
-          t.connectors.set('n', true)
-          t.connectors.set('e', true)
+          t.connectors.set("n", true)
+          t.connectors.set("e", true)
           break
         case "J":
-          t.connectors.set('n', true)
-          t.connectors.set('w', true)
+          t.connectors.set("n", true)
+          t.connectors.set("w", true)
           break
         case "7":
-          t.connectors.set('s', true)
-          t.connectors.set('w', true)
+          t.connectors.set("s", true)
+          t.connectors.set("w", true)
           break
         case "F":
-          t.connectors.set('s', true)
-          t.connectors.set('e', true)
+          t.connectors.set("s", true)
+          t.connectors.set("e", true)
           break
       }
       input.tiles[y][x] = t
@@ -87,13 +98,20 @@ const parseInput = (rawInput: string): Input => {
     const line = input.tiles[y]
 
     for (let x = 0; x < line.length; x++) {
-      if (x > 0) {
-        line[x].tiles.set('w', line[x-1])
-        line[x-1].tiles.set('e', line[x])
+      const tile = line[x]
+      if (x > 0 && tile.connectors.get("w")) {
+        const west = line[x - 1]
+        if (west.connectors.get("e")) {
+          tile.tiles.set("w", west)
+          west.tiles.set("e", tile)
+        }
       }
-      if (y > 0) {
-        input.tiles[y][x].tiles.set('n', input.tiles[y-1][x])
-        input.tiles[y-1][x].tiles.set('s', input.tiles[y][x])
+      if (y > 0 && tile.connectors.get("n")) {
+        const north = input.tiles[y - 1][x]
+        if (north.connectors.get("s")) {
+          tile.tiles.set("n", north)
+          north.tiles.set("s", tile)
+        }
       }
     }
   }
@@ -101,18 +119,62 @@ const parseInput = (rawInput: string): Input => {
   return input
 }
 
-const connectingTile = (t: Tile, cardinal: string): Record<boolean, Tile> {
-  
+const connectingTile = (t: Tile, from: string): [string, Tile] => {
+  for (const card of cardinals) {
+    if (card === from) {
+      continue
+    }
+
+    const tile = t.tiles.get(card)
+    if (tile) {
+      return [card, tile]
+    }
+  }
+
+  console.error("no connecting tile found", t)
+  throw "no connecting tile found"
+}
+
+const walkCircle = (t: Tile, card: string): string => {
+  let dist = 0
+  let tile = t
+  let firstCard = ""
+  card = opposite(card)
+
+  while (!tile.isStart || dist === 0) {
+    ;[card, tile] = connectingTile(tile, opposite(card))
+    // console.log("step", card, tile)
+    if (firstCard === "") {
+      firstCard = card
+    }
+
+    dist++
+    if (tile.distance > dist) {
+      tile.distance = dist
+    }
+  }
+
+  return firstCard
 }
 
 const part1 = (rawInput: string): number => {
   const input = parseInput(rawInput)
 
-  console.log(input.tiles)
+  const card = walkCircle(input.start as Tile, "")
+  walkCircle(input.start as Tile, card)
 
-  let total = 0
+  // console.log(input.tiles)
 
-  return total
+  let dist = 0
+  for (const line of input.tiles) {
+    for (const tile of line) {
+      if (tile.distance !== Number.MAX_SAFE_INTEGER && dist < tile.distance) {
+        dist = tile.distance
+      }
+    }
+  }
+
+  return dist
 }
 
 const part2 = (rawInput: string) => {
@@ -153,5 +215,5 @@ LJ...`,
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 })
