0808fbfa14d1d9e9645f1e5f820f46bd04c343e5
Paul van Santen <pvansanten@transip.nl>
2024-12-08 09:29:26 +0100

    Solve pt1

--- 2024/Sources/Day08.swift
+++ 2024/Sources/Day08.swift
@@ -1,10 +1,126 @@
 import Algorithms
 
 struct Day08: AdventDay {
+  struct AntennaType {
+    let antenna: String
+    var coords: [(Int, Int)]
+  }
+
+  struct Location: Hashable {
+    let antenna: String
+    var antiNode: Bool
+  }
+
   var data: String
 
+  var antennaMap: [[Location]] {
+    return data.split(separator: "\n").compactMap { 
+      $0.split(separator: "").compactMap {
+        if String($0) == "." {
+          return Location(antenna: "", antiNode: false)
+        }
+        return Location(antenna: String($0), antiNode: false)
+      }
+    }
+  }
+
+  var antennaTypes: [String:AntennaType] {
+    var mapping:[String:AntennaType] = [:]
+    for (y, line) in antennaMap.enumerated() {
+      for (x, loc) in line.enumerated() {
+        if loc.antenna == "" {
+          continue
+        }
+
+        var ant = mapping[loc.antenna]
+        if ant != nil {
+          ant!.coords += [(x, y)]
+          mapping[loc.antenna] = ant
+          continue
+        }
+
+        let newAnt = AntennaType(antenna: loc.antenna, coords: [(x, y)])
+        mapping[loc.antenna] = newAnt
+      }
+    }
+    return mapping
+  }
+
+  func antiNodes(c1: (Int, Int), c2: (Int, Int)) -> [(Int, Int)] {
+    return [
+      (2 * c2.0 - c1.0, 2 * c2.1 - c1.1),
+      (2 * c1.0 - c2.0, 2 * c1.1 - c2.1),
+    ]
+  }
+
+  func markAntiNode(map: inout [[Location]], c: (Int, Int)) {
+    if c.1 < 0 || c.1 >= map.count {
+      return
+    }
+    if c.0 < 0 || c.0 >= map[c.1].count {
+      return
+    }
+    map[c.1][c.0].antiNode = true
+  }
+
+  func markAntiNodes(antenna: AntennaType, map: inout [[Location]]) {
+    for (idx1, c1) in antenna.coords.enumerated() {
+      for (idx2, c2) in antenna.coords.enumerated() {
+        if idx1 == idx2 {
+          continue
+        }
+        for anti in antiNodes(c1: c1, c2: c2) {
+          markAntiNode(map: &map, c: anti)
+        }
+      }
+    }
+  }
+
+  func countAntiNodes(map: [[Location]]) -> Int {
+    var total = 0
+    for line in map {
+      for loc in line {
+        if loc.antiNode {
+          total += 1
+          continue
+        }
+      }
+    }
+    return total
+  }
+
+  func printMap(map: [[Location]], anti: Bool) {
+    var mp = ""
+    for line in map {
+      for loc in line {
+        if !anti && loc.antenna != "" {
+          mp += loc.antenna
+          continue
+        }
+        if loc.antiNode {
+         mp += "#"
+         continue
+        }
+        mp += "."
+      }
+      mp += "\n"
+    }
+    print(mp)
+  }
+
   func part1() -> Any {
-    return 0
+    var map = antennaMap
+    let tps = antennaTypes
+
+    // printMap(map: map, anti: true)
+
+    for (_, tp) in tps {
+      markAntiNodes(antenna: tp, map: &map)
+    }
+    // printMap(map: map, anti: true)
+    // printMap(map: map, anti: false)
+    // 356 too low
+    return countAntiNodes(map: map)
   }
 
   func part2() -> Any {
--- 2024/Tests/Day08.swift
+++ 2024/Tests/Day08.swift
@@ -7,16 +7,36 @@ import Testing
 struct Day08Tests {
   // Smoke test data provided in the challenge question
   let testData1 = """
-    
+    ............
+    ........0...
+    .....0......
+    .......0....
+    ....0.......
+    ......A.....
+    ............
+    ............
+    ........A...
+    .........A..
+    ............
+    ............
     """
 
   @Test func testPartD81() async throws {
     let challenge = Day08(data: testData1)
-    #expect(String(describing: challenge.part1()) == "0")
+    #expect(String(describing: challenge.part1()) == "14")
   }
 
   @Test func testPartD82() async throws {
     let challenge = Day08(data: testData1)
     #expect(String(describing: challenge.part2()) == "0")
+  }
+
+  @Test func testD7AntiNodes() async throws {
+    let challenge = Day08(data: testData1)
+    #expect(String(describing: challenge.antiNodes(c1: (4, 3), c2: (5, 5))) == "[(3, 1), (6, 7)]")
+    #expect(String(describing: challenge.antiNodes(c1: (5, 5), c2: (4, 3))) == "[(3, 1), (6, 7)]")
+    #expect(String(describing: challenge.antiNodes(c1: (4, 3), c2: (8, 4))) == "[(0, 2), (12, 5)]")
+    #expect(String(describing: challenge.antiNodes(c1: (5, 2), c2: (8, 1))) == "[(2, 3), (11, 0)]")
+    #expect(String(describing: challenge.antiNodes(c1: (8, 1), c2: (5, 2))) == "[(2, 3), (11, 0)]")
   }
 }
