3a462c6b91304af8eb90ecca4596bf7354eb498a
Paul van Santen <pvansanten@transip.nl>
2024-12-10 08:03:36 +0100

    d10 p1

---
+++ 2024/Sources/Day10.swift
@@ -0,0 +1,120 @@
+import Algorithms
+
+struct Day10: AdventDay {
+  struct Point : Hashable {
+    let x: Int
+    let y: Int
+
+    func add(p: Point) -> Point {
+      return Point(x: x + p.x, y: y + p.y)
+    }
+  }
+ 
+  var data: String
+
+  let top = 9
+
+  let directions: [Point] = [
+    Point(x: 0, y: -1),
+    Point(x: 1, y: 0),
+    Point(x: 0, y: 1),
+    Point(x: -1, y: 0),
+  ]
+
+  var map: [[Int]] {
+    return data.replacingOccurrences(of: ".", with: "1").split(separator: "\n").compactMap {
+      $0.split(separator: "").compactMap {
+        return Int($0)
+      }
+    }
+  }
+
+  func mapHeight(mp: [[Int]], p: Point) -> Int {
+    if !inBounds(mp: mp, p: p) {
+      return -1
+    }
+    return mp[p.y][p.x]
+  }
+
+  func inBounds(mp: [[Int]], p: Point) -> Bool {
+    return p.y >= 0 
+      && p.y < mp.count 
+      && p.x >= 0 
+      && p.x < mp[p.y].count
+  }
+
+  func findNeighbours(mp: [[Int]], p: Point, height: Int) -> [Point] {
+    var coords: [Point] = []
+    for dir in directions {
+      let n = p.add(p: dir)
+      if mapHeight(mp: mp, p: n) == height {
+        coords += [n]
+      }
+    }
+    return coords
+  }
+
+  func findHeight(mp: [[Int]], height: Int) -> [Point] {
+    var coords: [Point] = []
+    for (y, line) in mp.enumerated() {
+      for (x, hght) in line.enumerated() {
+        if hght == height {
+          coords += [Point(x: x, y: y)]
+        }
+      }
+    }
+    return coords
+  }
+
+  func walkToTop(mp: [[Int]], start: Point, tops: inout Set<Point>) {
+    let height = mapHeight(mp: mp, p: start)
+    if height == top {
+      // print("TOP", start)
+      tops.insert(start)
+      return
+    }
+    
+    var p = start
+    for h in height...top {
+      if h == top {
+        // print("TOP", p)
+        tops.insert(p)
+        return
+      }
+
+      // print("height", h)
+      let ns = findNeighbours(mp: mp, p: p, height: h + 1)
+      if ns.count > 1 {
+        for n in ns {
+          walkToTop(mp: mp, start: n, tops: &tops)
+        }
+        // Let our little spawned functions do the rest of the work
+        return
+      }
+      if ns.count > 0 {
+        p = ns[0]
+        continue
+      }
+      return
+    }
+  }
+
+  func part1() -> Any {
+    let mp = map
+    let starts = findHeight(mp: mp, height: 0)
+    print("START", mp)
+    var total = 0
+    for start in starts {
+      var tops: Set<Point> = []
+      walkToTop(mp: mp, start: start, tops: &tops)
+      print("start:", start, "tops:", tops)
+      total += tops.count
+    }
+
+    return total
+  }
+
+  func part2() -> Any {
+    return 0
+  }
+}
---
+++ 2024/Tests/Day10.swift
@@ -0,0 +1,60 @@
+import Testing
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+struct Day10Tests {
+  // Smoke test data provided in the challenge question
+  let testData1 = """
+    0123
+    1234
+    8765
+    9876
+    """
+  let testData2 = """
+    ...0...
+    ...1...
+    ...2...
+    6543456
+    7.....7
+    8.....8
+    9.....9
+    """
+  let testData3 = """
+    ..90..9
+    ...1.98
+    ...2..7
+    6543456
+    765.987
+    876....
+    987....
+    """
+  let testData4 = """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """
+
+
+  @Test func testPartD101() async throws {
+    var challenge = Day10(data: testData1)
+    #expect(String(describing: challenge.part1()) == "1")
+    challenge.data = testData2
+    #expect(String(describing: challenge.part1()) == "2")
+    challenge.data = testData3
+    #expect(String(describing: challenge.part1()) == "4")
+    challenge.data = testData4
+    #expect(String(describing: challenge.part1()) == "36")
+  }
+
+  @Test func testPartD102() async throws {
+    let challenge = Day10(data: testData1)
+    #expect(String(describing: challenge.part2()) == "")
+  }
+}
