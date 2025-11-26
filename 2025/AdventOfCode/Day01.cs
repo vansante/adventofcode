6a725c207bb2e68467bffc47b5b596e0e0b0434f
Paul van Santen <paul.vansanten@team.blue>
2025-11-26 16:57:38 +0100

    Test using 2015 day 1

--- 2025/AdventOfCode/Day01.cs
+++ 2025/AdventOfCode/Day01.cs
@@ -9,7 +9,41 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() {
+        int floor = 0;
+        for (int i = 0; i < _input.Length; i++) {
+            char c = _input[i];
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+            if (c == '(') {
+                floor++;
+            }
+            if (c == ')') {
+                floor--;
+            }
+        }
+
+        return new($"The last floor is {floor}");
+    }
+
+    public override ValueTask<string> Solve_2() {
+        int floor = 0;
+        int position = 0;
+        for (int i = 0; i < _input.Length; i++) {
+            position++;
+            char c = _input[i];
+
+            if (c == '(') {
+                floor++;
+            }
+            if (c == ')') {
+                floor--;
+            }
+
+            if (floor == -1) {
+                break;
+            }
+        }
+
+        return new($"The position is {position}");
+    }
 }
