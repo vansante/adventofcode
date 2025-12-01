7377f76914efa8f38b504233436d2deb4896b3fe
Paul van Santen <paul.vansanten@team.blue>
2025-12-01 09:07:50 +0100

    Complete 2025.1.1

--- 2025/AdventOfCode/Day01.cs
+++ 2025/AdventOfCode/Day01.cs
@@ -1,49 +1,90 @@
-﻿namespace AdventOfCode;
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
     private readonly string _input;
+    private readonly Input[] instructions;
+
+    enum Direction
+    {
+        Left,
+        Right
+    }
+
+    class Input
+    {
+        public Direction dir;
+        public int clicks;
+    }
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllText(InputFilePath).Trim();
+        string[] lines = _input.Split("\n");
+
+        instructions = new Input[lines.Length];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            Input instr = new Input
+            {
+                dir = Direction.Left,
+                clicks = 0
+            };
+
+            if (line[0] == 'R')
+            {
+                instr.dir = Direction.Right;
+            }
+
+            try
+            {
+                instr.clicks = Int32.Parse(line[1..]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse integer '{line[1..]}'");
+                continue;
+            }
+
+            instructions[i] = instr;
+        }
     }
 
     public override ValueTask<string> Solve_1() {
-        int floor = 0;
-        for (int i = 0; i < _input.Length; i++) {
-            char c = _input[i];
+        // Console.WriteLine($"{instructions[2].dir} {instructions[2].clicks}");
 
-            if (c == '(') {
-                floor++;
+        int zeroCount = 0;
+        int dial = 50;
+        for (int i = 0; i < instructions.Length; i++) {
+            Input instr = instructions[i];
+
+            dial += 100;
+            if (instr.dir == Direction.Left)
+            {
+                dial -= instr.clicks;
+            } else
+            {
+                dial += instr.clicks;
             }
-            if (c == ')') {
-                floor--;
+            dial %= 100;
+
+            if (dial == 0)
+            {
+                zeroCount++;
             }
+
+            Console.WriteLine($"{i} dial {dial} zeroes: {zeroCount}");
         }
 
-        return new($"The last floor is {floor}");
+        return new($"Zero was reached {zeroCount} times");
     }
 
     public override ValueTask<string> Solve_2() {
-        int floor = 0;
-        int position = 0;
-        for (int i = 0; i < _input.Length; i++) {
-            position++;
-            char c = _input[i];
-
-            if (c == '(') {
-                floor++;
-            }
-            if (c == ')') {
-                floor--;
-            }
-
-            if (floor == -1) {
-                break;
-            }
-        }
-
-        return new($"The position is {position}");
+        return new($"The answer is ");
     }
 }
