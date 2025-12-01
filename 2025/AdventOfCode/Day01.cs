1637f4afb142ca123a4e3547c07850e34219fc97
Paul van Santen <paul.vansanten@team.blue>
2025-12-01 10:42:25 +0100

    Part 2

--- 2025/AdventOfCode/Day01.cs
+++ 2025/AdventOfCode/Day01.cs
@@ -6,6 +6,9 @@ namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
+    public const int DialSize = 100;
+    public const int DialStart = 50;
+
     private readonly string _input;
     private readonly Input[] instructions;
 
@@ -41,29 +44,19 @@ public class Day01 : BaseDay
                 instr.dir = Direction.Right;
             }
 
-            try
-            {
-                instr.clicks = Int32.Parse(line[1..]);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine($"Unable to parse integer '{line[1..]}'");
-                continue;
-            }
+            instr.clicks = Int32.Parse(line[1..]);
 
             instructions[i] = instr;
         }
     }
 
     public override ValueTask<string> Solve_1() {
-        // Console.WriteLine($"{instructions[2].dir} {instructions[2].clicks}");
-
         int zeroCount = 0;
         int dial = 50;
         for (int i = 0; i < instructions.Length; i++) {
             Input instr = instructions[i];
 
-            dial += 100;
+            dial += DialSize;
             if (instr.dir == Direction.Left)
             {
                 dial -= instr.clicks;
@@ -71,20 +64,64 @@ public class Day01 : BaseDay
             {
                 dial += instr.clicks;
             }
-            dial %= 100;
+            dial %= DialSize;
 
             if (dial == 0)
             {
                 zeroCount++;
             }
-
-            Console.WriteLine($"{i} dial {dial} zeroes: {zeroCount}");
         }
 
         return new($"Zero was reached {zeroCount} times");
     }
 
+    public static int DialModulo(int dial)
+    {
+        return ((dial % DialSize) + DialSize) % DialSize;
+    }
+
     public override ValueTask<string> Solve_2() {
-        return new($"The answer is ");
+        int zeroCount = 0;
+        int dial = DialStart;
+        for (int i = 0; i < instructions.Length; i++) {
+            Input instr = instructions[i];
+
+            // Add the amount of full cycles (will definitely hit zero)
+            zeroCount += instr.clicks / DialSize;
+
+            if (instr.dir == Direction.Left)
+            {
+                // If we are not currently on zero, and we will pass zero in the current pass remainder, add zero
+                if (dial != 0 && dial - DialModulo(instr.clicks) < 0)
+                {
+                    zeroCount++;
+                }
+                // Add the full amount of clicks
+                dial -= instr.clicks;
+            } else
+            {
+                // If we will pass zero in the current pass remainder, add zero
+                if (dial + DialModulo(instr.clicks) > 100)
+                {
+                    zeroCount++;
+                }
+                // Add the full amount of clicks
+                dial += instr.clicks;
+            }
+
+            // Set the dial back to 0-99
+            dial = DialModulo(dial);
+
+            // If we end on a zero, add it:
+            if (dial == 0)
+            {
+                zeroCount++;
+            }
+        }
+
+
+        // < 7426
+        // > 6350
+        return new($"Zero was hit {zeroCount} times");
     }
 }
